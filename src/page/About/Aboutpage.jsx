f990e9b0808db02c4dd4fa37b13f921d3b430ff2
exwizzar <exwizzar@gmail.com>
2022-11-06 16:24:47 +1000

    Nailed footer to the bottom of the site and designed the About block

--- src/page/About/Aboutpage.jsx
+++ src/page/About/Aboutpage.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Rectangle from '../../components/UI/rectangle/Rectangle'
+import './aboutStyle.scss'
 
 const Aboutpage = () => {
   return (
-    <div>Aboutpage</div>
+    <div className='about'>
+    <Rectangle>О Компании</Rectangle>
+    <p>RitmStyle - ipsum dolor sit amet consectetur adipisicing elit. Dolore magnam ut aperiam reprehenderit perspiciatis, illum recusandae non, 
+    tempora alias praesentium eius voluptatem, at tempore assumenda autem blanditiis suscipit aspernatur iure.Lorem ipsum dolor sit amet 
+    consectetur adipisicing elit. Dolore magnam ut aperiam reprehenderit perspiciatis, illum recusandae non, tempora alias praesentium eius voluptatem
+    , at tempore assumenda autem blanditiis suscipit aspernatur iure.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore magnam ut aperiam 
+    reprehenderit perspiciatis, illum recusandae non, tempora alias praesentium eius voluptatem, at tempore assumenda autem blanditiis suscipit aspernatur 
+    iure.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore magnam ut aperiam reprehenderit perspiciatis, illum recusandae non, tempora alias
+    praesentium eius voluptatem, at tempore assumenda autem blanditiis suscipit aspernatur iure.<br/>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore 
+    magnam ut aperiam reprehenderit perspiciatis, illum recusandae non, tempora alias praesentium eius voluptatem, at tempore assumenda autem blanditiis suscipit
+    aspernatur iure.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore magnam ut aperiam reprehenderit perspiciatis, illum recusandae non, tempora 
+    alias praesentium eius voluptatem, at tempore assumenda autem blanditiis suscipit aspernatur iure.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Dolore magnam ut aperiam reprehenderit perspiciatis, illum recusandae non, tempora alias praesentium eius voluptatem, at tempore assumenda autem blanditiis
+    suscipit aspernatur iure.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore magnam ut aperiam reprehenderit perspiciatis, illum recusandae non,
+    tempora alias praesentium eius voluptatem, at tempore assumenda autem blanditiis suscipit aspernatur iure.Lorem ipsum dolor sit amet consectetur adipisicing
+    elit. Dolore magnam ut aperiam reprehenderit perspiciatis, illum recusandae non, tempora alias praesentium eius voluptatem, at tempore assumenda autem blanditiis
+    uscipit aspernatur iure.</p>
+    </div>
   )
 }
 
