428cc7a582d1acc6a3fd4afa9aa889cb9e76e632
exwizzar <exwizzar@gmail.com>
2022-11-08 17:48:23 +1000

    add flex-wrap in styles

--- src/page/main/Mainpage.jsx
+++ src/page/main/Mainpage.jsx
@@ -15,7 +15,7 @@ const Mainpage = ({setModalActive}) => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 10000,
@@ -100,22 +100,22 @@ const Mainpage = ({setModalActive}) => {
       <div className='about'>
         <Heading>Об RitmStyle</Heading>
         
-        <div className='about-massage'>
-          <div className='about-massage__info'>
+        <div className='about-block'>
+          <div className='about-massage'>
             <h2 className='about-massage__title'>RitmStyle массаж</h2>
             <p className='about-massage__text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in purus, sed tellus eget mattis nibh quam. Eu ornare nunc, id est. Erat consectetur etiam a sit diam in imperdiet amet. Diam nisl, ipsum suscipit amet. Eleifend amet habitasse proin quis adipiscing.
             Nisl convallis mauris in consequat. Sit ac vitae posuere maecenas dictumst quam. Felis amet diam, non augue massa. Egestas molestie lobortis rhoncus, elit nulla nisl. Habitant tortor at tempor.</p>
           </div>
-            <div className='about-massage__img'>
+            <div className='about-block__img'>
               <img src={swimGrl} alt="" />
             </div>
         </div>
 
-        <div className='about-massage'>
-            <div className='about-massage__img'>
+        <div className='about-block'>
+            <div className='about-block__img'>
               <img src={swimGrl} alt="" />
             </div>
-          <div className='about-massage__info'>
+          <div className='about-massage'>
             <h2 className='about-massage__title'>RitmStyle массаж</h2>
             <p className='about-massage__text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in purus, sed tellus eget mattis nibh quam. Eu ornare nunc, id est. Erat consectetur etiam a sit diam in imperdiet amet. Diam nisl, ipsum suscipit amet. Eleifend amet habitasse proin quis adipiscing.
             Nisl convallis mauris in consequat. Sit ac vitae posuere maecenas dictumst quam. Felis amet diam, non augue massa. Egestas molestie lobortis rhoncus, elit nulla nisl. Habitant tortor at tempor.</p>
