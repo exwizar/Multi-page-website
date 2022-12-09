b9511dc1b47b58f0b922b589b3a83ce97aaa8922
exwizzar <exwizzar@gmail.com>
2022-12-10 01:09:42 +1000

    Added the function of data mandrel and forms in telegram

--- src/components/MudalBlock/Modal.jsx
+++ src/components/MudalBlock/Modal.jsx
@@ -1,28 +1,47 @@
 import React from 'react'
 import './modal.scss'
 import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
 
 const Modal = ({active, setActive}) => {
-  const { register, handleSubmit, formState: { errors, isValid } } = useForm();
-  const onSubmit = data => console.log(data);
-  console.log(errors);
+  const TOKEN = "5824337953:AAH0l341FlZ8GtZuvTlwbjiTDoLdRmida88";
+  const CHAT_ID = "-1001815132509";
+  const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+  const { register, getValues, handleSubmit, reset, formState: { isValid } } = useForm();
+
+  function subForm(e) {
+    e.preventDefault();
+    setActive(false)
+    const info = getValues()
+    let message = `<b>Заявка с сайта!</b>\n<b>Отправитель: </b> ${info.FirstName}\n<b>Номер телефона: </b> ${info.MobileNumber}`
+
+    axios.post(URL_API, {
+      chat_id: CHAT_ID,
+      parse_mode: 'html',
+      text: message
+    })
+    reset({
+      FirstName: '',
+      MobileNumber: ''
+    })
+  }
 
   return (
     <div className={active ? 'modal active' : 'modal'} onClick={() => setActive(false)}>
         <div className={active ? 'modal__content active' : 'modal__content'} onClick={e => e.stopPropagation()}>
 
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit()}>
             <span className="text-center">Запись на сеанс</span>
             <div className="input-container">
-              <input type="text" required="" {...register("First name", {required: true, maxLength: 80})}/>
+              <input type="text" required="" {...register("FirstName", {required: true, maxLength: 80})}/>
               <label>Имя</label>		
             </div>
             <div className="input-container">		
-              <input type="tel" required="" {...register("Mobile number", {required: true, minLength: 6, maxLength: 12})} />
+              <input type="tel" required="" {...register("MobileNumber", {required: true, minLength: 6, maxLength: 12})} />
               <label>Номер телефона</label>
             </div>
-              <input type="Submit" className="btn" disabled={!isValid} onClick={() => setActive(false)}/>
+              <input type="Submit" className="btn" disabled={!isValid} onClick={subForm}/>
         </form>	
 
         </div>
